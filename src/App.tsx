380c06d29158335c01cb1e57b3f2b8f911ae5b50
Stathis Moraitidis <emoraitidis@gmail.com>
2024-02-10 15:14:08 +0000

    feat: automerge quickstart example

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {default as A} from "@automerge/automerge/next"
+import {AutomergeUrl} from '@automerge/automerge-repo'
+import {useDocument} from '@automerge/automerge-repo-react-hooks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-          Tailwind says hello world!
-        </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface CounterDoc {
+    counter: A.Counter
 }
 
-export default App;
+function App({docUrl}: {docUrl: AutomergeUrl}) {
+  const [doc, changeDoc] = useDocument<CounterDoc>(docUrl) 
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => changeDoc((d) => d.counter.increment(1))}>
+          count is { doc && doc.counter.value }
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
