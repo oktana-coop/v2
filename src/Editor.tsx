5a05a2db2f61fdac954b5ccdda1ad26052c18939
teomrd <mironidis.gr@gmail.com>
2024-02-24 19:20:58 +0000

    feat: on cmd+s save changes

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDocument } from '@automerge/automerge-repo-react-hooks';
 import { AutomergeUrl } from '@automerge/automerge-repo';
-import { default as Automerge } from '@automerge/automerge/next';
+import {
+  default as Automerge,
+  decodeChange,
+  getAllChanges,
+  view,
+} from '@automerge/automerge/next';
 
 interface Document {
   doc: Automerge.Doc<string>;
@@ -10,10 +15,17 @@ interface Document {
 export function Editor({ docUrl }: { docUrl: AutomergeUrl }) {
   const [value, changeValue] = React.useState<string>('');
   const [document, changeDocument] = useDocument<Document>(docUrl);
+  const [hashes, setHashes] = React.useState<Array<string>>([]);
 
   useEffect(() => {
     if (document) {
       changeValue(document.doc || '');
+
+      const changes = getAllChanges(document);
+      const decodedChanges = changes.map((change) => decodeChange(change));
+      console.log('decodedChanges 👉', decodedChanges);
+      const hashes = decodedChanges.map((change) => change.hash);
+      setHashes(hashes);
     }
   }, [document]);
 
@@ -27,6 +39,30 @@ export function Editor({ docUrl }: { docUrl: AutomergeUrl }) {
     });
   };
 
+  const onSave = () => {
+    const commitMessage = prompt('Save your changes with a message..');
+    console.log('commitMessage 👉', commitMessage);
+
+    changeDocument((doc) => {
+      doc.doc = value;
+    });
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    // cmd/ctrl + s
+    if (e.metaKey && e.key === 's') {
+      e.preventDefault();
+      onSave();
+    }
+  };
+
+  const handleHashClick = (hash: string) => {
+    if (document) {
+      const docView = view(document, [hash]);
+      console.log(docView.doc);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center w-full m-2">
       <textarea
@@ -36,8 +72,19 @@ export function Editor({ docUrl }: { docUrl: AutomergeUrl }) {
         className="focus:shadow-inner w-3/5 h-full resize-none p-5 text-black bg-white rounded-md border-none outline-none border-gray-400"
         autoFocus
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
         onBlur={handleBlur}
       />
+      <div className="flex-auto w-32 p-5 h-full break-words text-black bg-white">
+        <div>
+          <h1>History</h1>
+          {hashes.map((hash) => (
+            <div key={hash} onClick={() => handleHashClick(hash)}>
+              {hash}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
