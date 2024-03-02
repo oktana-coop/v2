042e2523b48796dfe6987d7d417737300cf767dc
Aris Goudouras <aris.gk@gmail.com>
2024-03-02 19:42:32 +0200

    Revert "chore: setup storybook (#32)"
    This reverts commit 865977de13c132b77fe2cab49659fb0db9af71d4.

--- src/components/typography/Heading1.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react';
-
-export type Heading1Props = { children: ReactNode };
-
-// TODO: Add proper styles
-export const Heading1 = ({ children }: Heading1Props) => <h1>{children}</h1>;
