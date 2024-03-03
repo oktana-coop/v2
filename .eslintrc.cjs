b0c305671e3ccd5b72e03a3704bf259b64b99bba
Aris Goudouras <aris.gk@gmail.com>
2024-03-03 08:52:12 +0200

    chore: update ESLint config formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,14 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
@@ -11,4 +18,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
