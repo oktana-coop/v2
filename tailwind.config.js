b2e4c94a0f40e1e8fc26b431f51f1b3c27bc0be9
Aris Goudouras <aris.gk@gmail.com>
2024-03-16 09:11:18 +0200

    chore: use custom purple palette (#44)
    Closes #38

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          50: '#F6F2FF',
+          100: '#EEE8FF',
+          200: '#DED4FF',
+          300: '#C8B1FF',
+          400: '#AD85FF',
+          500: '#9352FF',
+          600: '#8730F7',
+          700: '#791EE3',
+          800: '#6518BF',
+          900: '#53169C',
+          950: '#340B6A',
+        },
+      },
+    },
   },
   plugins: [],
 };
