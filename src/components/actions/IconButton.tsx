6a0b638e451eceade7bc2331dbf380a5f73a429a
Aris Goudouras <aris.gk@gmail.com>
2024-03-20 17:24:34 +0200

    feat: update button styles for dark theme (#49)

--- src/components/actions/IconButton.tsx
+++ src/components/actions/IconButton.tsx
@@ -1,12 +1,17 @@
-import { Button } from './Button';
+import { Button, type ButtonColor } from './Button';
 import type { ReactNode } from 'react';
 
 export type IconButtonProps = {
   icon: ReactNode;
+  color?: ButtonColor;
 };
 
-export const IconButton = ({ icon }: IconButtonProps) => (
-  <Button variant="plain" className="!px-0 !py-0 !sm:px-0 !sm:py-0">
+export const IconButton = ({ icon, color }: IconButtonProps) => (
+  <Button
+    variant="plain"
+    color={color}
+    className="!px-0 !py-0 !sm:px-0 !sm:py-0"
+  >
     {icon}
   </Button>
 );
