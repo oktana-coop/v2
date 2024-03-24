945d3308a50e64c59ea851ac21390a6a84ed2f81
Aris Goudouras <aris.gk@gmail.com>
2024-03-24 19:41:10 +0200

    test: fix section header stories

--- src/pages/Options/SectionHeader.stories.tsx
+++ src/pages/Options/SectionHeader.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import type { ComponentProps } from 'react';
 
+import { SunIcon } from '../../components/icons';
 import { SectionHeader } from './SectionHeader';
 
 const meta: Meta<typeof SectionHeader> = {
@@ -13,8 +14,17 @@ const meta: Meta<typeof SectionHeader> = {
 
 type Story = StoryObj<ComponentProps<typeof SectionHeader>>;
 
-export const Default: Story = {
-  render: () => <SectionHeader />,
+export const WithIcon: Story = {
+  args: {
+    icon: SunIcon,
+    heading: 'Theme',
+  },
+};
+
+export const HeadingOnly: Story = {
+  args: {
+    heading: 'Heading',
+  },
 };
 
 export default meta;
