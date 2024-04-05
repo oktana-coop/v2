47d3a8263ac0e0a68c0ae895ae6381a6643c1537
teomrd <mironidis.gr@gmail.com>
2024-04-05 22:14:36 +0100

    feat: implement Modal component
    based on Dialog from Catalyst which is left untouched

---
+++ src/renderer/src/components/dialogs/Modal.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogDescription,
+  DialogTitle,
+} from './Dialog';
+
+type ModalProps = {
+  title: string;
+  description?: string;
+  isOpen?: boolean;
+  primaryButton?: React.ReactNode;
+  secondaryButton?: React.ReactNode;
+  children?: React.ReactNode;
+};
+
+export const Modal = ({
+  title,
+  description,
+  isOpen: isOpenProp = false,
+  primaryButton,
+  secondaryButton,
+  children,
+}: ModalProps) => {
+  const [isOpen, setIsOpen] = useState(isOpenProp);
+  useEffect(() => {
+    setIsOpen(isOpenProp);
+  }, [isOpenProp]);
+
+  return (
+    <Dialog open={isOpen} onClose={(e) => console.log(e)}>
+      <DialogTitle>{title}</DialogTitle>
+      {description && <DialogDescription>{description}</DialogDescription>}
+      {children && <DialogBody>{children}</DialogBody>}
+      <DialogActions>
+        {secondaryButton}
+        {primaryButton}
+      </DialogActions>
+    </Dialog>
+  );
+};
