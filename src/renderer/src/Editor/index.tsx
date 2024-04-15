32c898b14ecc26cb3913a71f024e5c3b668f08a9
teomrd <mironidis.gr@gmail.com>
2024-04-15 12:54:46 +0100

    feat: explicitly create a document

--- src/renderer/src/Editor/index.tsx
+++ src/renderer/src/Editor/index.tsx
@@ -1,8 +1,36 @@
+import * as Automerge from '@automerge/automerge';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { repo } from '../automerge/repo';
+import { Button } from '../components/actions/Button';
 import { Link } from '../components/actions/Link';
+import { PenIcon } from '../components/icons';
 import { PersonalFile } from '../components/illustrations/PersonalFile';
+import { AutomergeUrl } from '@automerge/automerge-repo';
+
+const persistDocumentUrl = (docUrl: AutomergeUrl) => {
+  const currentDocUrls = localStorage.getItem('docUrls');
+  if (currentDocUrls) {
+    const currentDocs = JSON.parse(currentDocUrls);
+    localStorage.setItem(
+      'docUrls',
+      JSON.stringify({
+        ...currentDocs,
+        [docUrl]: docUrl,
+      })
+    );
+  } else {
+    localStorage.setItem(
+      'docUrls',
+      JSON.stringify({
+        [docUrl]: docUrl,
+      })
+    );
+  }
+};
 
 export const EditorIndex = () => {
+  const navigate = useNavigate();
   const [docIds, setDocIds] = useState<Array<string>>([]);
 
   useEffect(() => {
@@ -13,20 +41,43 @@ export const EditorIndex = () => {
     }
   }, []);
 
+  const handleDocumentCreation = () => {
+    const handle = repo.create<{ doc?: Automerge.Doc<string> }>();
+    const newDocUrl = handle.url;
+    // temporary workaround to persist the document url
+    // until we figure out how to handle existing documents persistence
+    persistDocumentUrl(newDocUrl);
+    navigate(`/edit/${newDocUrl}`);
+  };
+
   return (
     <div className="flex-auto flex">
-      <div className="h-full w-2/5 grow-0 p-5 overflow-y-scroll ">
-        <h2>Your docs</h2>
-        {docIds.map((docId) => (
-          <div className="text-left" key={docId}>
-            <Link to={`/edit/${docId}`}>{docId}</Link>
-          </div>
-        ))}
-      </div>
+      {docIds.length === 0 && (
+        <div className="h-full w-2/5 grow-0 p-5 overflow-y-scroll ">
+          <h2>Your docs</h2>
+          {docIds.map((docId) => (
+            <div className="text-left" key={docId}>
+              <Link to={`/edit/${docId}`}>{docId}</Link>
+            </div>
+          ))}
+        </div>
+      )}
       <div className="h-full w-full grow flex flex-col items-center justify-center">
         <h2 className="text-2xl">Welcome to v2 👋</h2>
         <p className="text-gray-500">
-          👈 Pick one documents of the list to continue editing 😉
+          {docIds.length === 0
+            ? '👈 Pick one documents of the list to continue editing 😉'
+            : 'Create a new document and explore the world of versioning'}
+        </p>
+        <p className="m-5">
+          <Button
+            onClick={handleDocumentCreation}
+            variant="solid"
+            color="purple"
+          >
+            <PenIcon />
+            Create document
+          </Button>
         </p>
         <PersonalFile />
       </div>
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,56 +1,19 @@
-import * as A from '@automerge/automerge';
-import { AutomergeUrl } from '@automerge/automerge-repo';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-import { DocHandle, isValidAutomergeUrl } from '@automerge/automerge-repo';
+import { DocHandle } from '@automerge/automerge-repo';
 import './index.css';
 
 import { RepoContext } from '@automerge/automerge-repo-react-hooks';
 import { repo } from './automerge';
 import { ThemeProvider } from './personalization/theme';
 
-const persistDocumentUrl = (docUrl: AutomergeUrl) => {
-  const currentDocUrls = localStorage.getItem('docUrls');
-  if (currentDocUrls) {
-    const currentDocs = JSON.parse(currentDocUrls);
-    localStorage.setItem(
-      'docUrls',
-      JSON.stringify({
-        ...currentDocs,
-        [docUrl]: docUrl,
-      })
-    );
-  } else {
-    localStorage.setItem(
-      'docUrls',
-      JSON.stringify({
-        [docUrl]: docUrl,
-      })
-    );
-  }
-};
-
 declare global {
   interface Window {
     handle: DocHandle<unknown>;
   }
 }
-
-const queryParams = new URLSearchParams(window.location.search);
-const rootDocUrl = queryParams.get('docUrl');
-
-let handle;
-if (isValidAutomergeUrl(rootDocUrl)) {
-  handle = repo.find(rootDocUrl);
-} else {
-  handle = repo.create<{ doc?: A.Doc<string> }>();
-  handle.change((d) => (d.doc = ''));
-}
-const docUrl = handle.url;
-// temporary solution to persist the document URLs
-persistDocumentUrl(docUrl);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
