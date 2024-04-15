329fe884a94bd92015fb9144ef1ad3df1ac12d26
teomrd <mironidis.gr@gmail.com>
2024-04-15 12:21:23 +0100

    feat: revamp History index

--- src/renderer/src/pages/History/index.tsx
+++ src/renderer/src/pages/History/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from '../../components/actions/Link';
+import { PersonalFile } from '../../components/illustrations/PersonalFile';
 
 export const HistoryIndex = () => {
   const [docIds, setDocIds] = useState<Array<string>>([]);
@@ -13,14 +14,22 @@ export const HistoryIndex = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Welcome to v2 👋</h1>
-      <div>
+    <div className="flex-auto flex">
+      <div className="h-full w-2/5 grow-0 p-5 overflow-y-scroll ">
+        <h2>Your docs</h2>
         {docIds.map((docId) => (
-          <div key={docId}>
+          <div className="text-left" key={docId}>
             <Link to={`/history/${docId}`}>{docId}</Link>
           </div>
         ))}
+      </div>
+      <div className="h-full w-full grow flex flex-col items-center justify-center">
+        <h2 className="text-2xl">Welcome to v2 👋</h2>
+        <p className="text-gray-500">
+          👈 You can explore a documents' editing history by picking up one of
+          the list 😉
+        </p>
+        <PersonalFile />
       </div>
     </div>
   );
