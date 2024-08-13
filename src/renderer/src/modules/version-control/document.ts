77d44d93f449fea51b251b7baf9e7add75ad8773
Aris Goudouras <aris.gk@gmail.com>
2024-08-13 18:25:26 +0300

    refactor: extract document exports in a separate file

---
+++ src/renderer/src/modules/version-control/document.ts
@@ -0,0 +1,22 @@
+import * as Automerge from '@automerge/automerge/next';
+
+export type VersionedDocument = {
+  title: Automerge.Doc<string>;
+  content: Automerge.Doc<string>;
+};
+
+// Commit is a special type of an (automerge) change that
+// strictly has a message and a time
+export type Commit = {
+  hash: string;
+  message: string;
+  time: Date;
+};
+
+// this is a TS type guard to check if a change is a commit
+export const isCommit = (
+  change: Automerge.DecodedChange | Commit
+): change is Commit => {
+  // we make the rules!
+  return Boolean(change.message && change.time);
+};
