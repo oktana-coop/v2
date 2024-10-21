4733c828ea7e7ed0e83d693c0f5b7cc8ac8746fa
Aris Goudouras <aris@aris-mbp.local>
2024-10-21 13:37:10 +0300

    chore: skip bundling for selected dependencies

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -12,6 +12,16 @@ export default defineConfig(({ mode }) => {
       define: {
         'process.env': env,
       },
+      build: {
+        rollupOptions: {
+          external: [
+            '@automerge/automerge-wasm',
+            '@automerge/automerge',
+            'bufferutil',
+            'ws',
+          ],
+        },
+      },
     },
     preload: {
       build: {
@@ -28,6 +38,16 @@ export default defineConfig(({ mode }) => {
       // reference process.env.
       define: {
         'process.env': env,
+      },
+      build: {
+        rollupOptions: {
+          external: [
+            '@automerge/automerge-wasm',
+            '@automerge/automerge',
+            'bufferutil',
+            'ws',
+          ],
+        },
       },
       plugins: [topLevelAwait(), wasm(), react()],
     },
