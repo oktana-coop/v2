bc37d18a7ee790e71554896dc87905d387b1ca23
Aris Goudouras <aris.gk@gmail.com>
2024-10-24 10:10:54 +0300

    Revert "chore: update vite versions and config"
    This reverts commit c655cf10ab80ade2cac86358024fa3ca3deb5817.

---
+++ electron.vite.config.ts
@@ -0,0 +1,55 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'electron-vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode);
+
+  return {
+    publicDir: false,
+    main: {
+      define: {
+        'process.env': env,
+      },
+      build: {
+        rollupOptions: {
+          external: [
+            '@automerge/automerge-wasm',
+            '@automerge/automerge',
+            'bufferutil',
+            'ws',
+          ],
+        },
+      },
+    },
+    preload: {
+      build: {
+        rollupOptions: {
+          output: {
+            format: 'cjs', // Set output format to CommonJS
+            entryFileNames: 'index.js', // Customize the output file name
+          },
+        },
+      },
+    },
+    renderer: {
+      // Needed in the renderer because automerge dependencies
+      // reference process.env.
+      define: {
+        'process.env': env,
+      },
+      build: {
+        rollupOptions: {
+          external: [
+            '@automerge/automerge-wasm',
+            '@automerge/automerge',
+            'bufferutil',
+            'ws',
+          ],
+        },
+      },
+      plugins: [topLevelAwait(), wasm(), react()],
+    },
+  };
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,29 @@
-import path from 'node:path';
-
+// vite.config.ts
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import electron from 'vite-plugin-electron/simple';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    topLevelAwait(),
-    wasm(),
-    react(),
-    electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: 'src/main/index.ts',
-        vite: {
-          build: {
-            outDir: 'dist/main',
-            target: 'esnext',
-          },
-        },
-      },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'src/preload/index.ts'),
-        vite: {
-          build: {
-            outDir: 'dist/preload',
-            rollupOptions: {
-              output: {
-                format: 'cjs', // Set output format to CommonJS
-                entryFileNames: 'index.js', // Customize the output file name
-              },
-            },
-          },
-        },
-      },
-      // Polyfill the Electron and Node.js API for Renderer process.
-      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer:
-        process.env.NODE_ENV === 'test'
-          ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-            undefined
-          : {},
-    }),
-  ],
+  // process.env definition is needed because of:
+  // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
+  define: {
+    'process.env': {},
+  },
+  optimizeDeps: {
+    // This is necessary because otherwise `vite dev` includes two separate
+    // versions of the JS wrapper. This causes problems because the JS
+    // wrapper has a module level variable to track JS side heap
+    // allocations, and initializing this twice causes horrible breakage
+    exclude: [
+      '@automerge/automerge-wasm',
+      '@automerge/automerge-wasm/bundler/bindgen_bg.wasm',
+      '@syntect/wasm',
+    ],
+  },
+  plugins: [topLevelAwait(), wasm(), react()],
+  worker: {
+    format: 'es',
+    plugins: () => [wasm()],
+  },
 });
