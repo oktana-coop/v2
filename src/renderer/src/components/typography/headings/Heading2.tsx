25bf31ad89ae9844401403e8b1db71389e03e12c
Aris Goudouras <aris.gk@gmail.com>
2025-01-15 12:55:11 +0200

    feat: use custom paragraph styles

--- src/renderer/src/components/typography/headings/Heading2.tsx
+++ src/renderer/src/components/typography/headings/Heading2.tsx
@@ -3,7 +3,7 @@ import { clsx } from 'clsx';
 import { HeadingProps } from './types';
 
 export const classes =
-  'font-bold text-2xl text-black dark:text-white text-opacity-90 dark:text-opacity-90';
+  'font-bold text-2xl text-black dark:text-white text-opacity-90 dark:text-opacity-90 mb-4';
 
 export const Heading2 = ({ children, className }: HeadingProps) => (
   <h2 className={clsx(classes, className)}>{children}</h2>
--- src/renderer/src/components/typography/headings/Heading4.tsx
+++ src/renderer/src/components/typography/headings/Heading4.tsx
@@ -3,7 +3,7 @@ import { clsx } from 'clsx';
 import { HeadingProps } from './types';
 
 export const classes =
-  'font-bold text-base text-black dark:text-white text-opacity-90 dark:text-opacity-90';
+  'font-bold text-base text-black dark:text-white text-opacity-90 dark:text-opacity-90 mb-4';
 
 export const Heading4 = ({ children, className }: HeadingProps) => (
   <h4 className={clsx(classes, className)}>{children}</h4>
