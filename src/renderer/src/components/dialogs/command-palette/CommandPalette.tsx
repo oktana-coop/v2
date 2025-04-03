6de57d355900d3daf4954c9d3b4df265d17dda35
teomrd <mironidis.gr@gmail.com>
2025-04-03 09:32:53 +0100

    feat(cmd-plt): make it look great on dark mode

--- src/renderer/src/components/dialogs/command-palette/CommandPalette.tsx
+++ src/renderer/src/components/dialogs/command-palette/CommandPalette.tsx
@@ -61,7 +61,7 @@ export const CommandPalette = () => {
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto p-4 sm:p-6 md:p-20">
         <DialogPanel
           transition
-          className="mx-auto max-w-2xl transform divide-y divide-gray-500/10 overflow-hidden rounded-xl bg-white/80 shadow-2xl ring-1 ring-black/5 backdrop-blur backdrop-filter transition-all data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+          className="mx-auto max-w-2xl transform divide-y divide-gray-500/10 overflow-hidden bg-white/80 shadow-2xl ring-1 ring-black/5 backdrop-blur backdrop-filter transition-all data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in dark:bg-zinc-900/80"
         >
           <Combobox
             onChange={(item) => {
@@ -71,7 +71,7 @@ export const CommandPalette = () => {
             <div className="grid grid-cols-1">
               <ComboboxInput
                 autoFocus
-                className="col-start-1 row-start-1 h-12 w-full bg-transparent p-4 text-base text-gray-900 outline-none placeholder:text-gray-500 sm:text-sm"
+                className="col-start-1 row-start-1 h-12 w-full bg-transparent p-4 text-base text-gray-900 outline-none placeholder:text-gray-500 sm:text-sm dark:text-gray-100 dark:placeholder:text-gray-300"
                 placeholder="Search..."
                 onChange={(event) => setQuery(event.target.value)}
                 onBlur={() => setQuery('')}
@@ -86,18 +86,18 @@ export const CommandPalette = () => {
               >
                 <li className="p-4">
                   {query === '' && (
-                    <h2 className="mb-2 mt-4 text-xs font-semibold text-gray-900">
+                    <h2 className="mb-2 mt-4 text-xs font-semibold text-gray-900 dark:text-gray-300">
                       Matcing documents
                     </h2>
                   )}
-                  <ul className="text-sm text-gray-700">
+                  <ul className="text-sm text-gray-700 dark:text-gray-400">
                     {(query === '' ? recent : filteredProjects).map(
                       (project) => (
                         <ComboboxOption
                           as="li"
                           key={project.id}
                           value={project}
-                          className="group flex cursor-default select-none items-center rounded-md px-2 py-2 data-[focus]:bg-gray-900/5 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                          className="group flex cursor-default select-none items-center rounded-md px-2 py-2 data-[focus]:bg-gray-900/5 data-[focus]:text-gray-900 data-[focus]:outline-none dark:data-[focus]:bg-gray-300/5 dark:data-[focus]:text-gray-100"
                         >
                           <span className="flex-auto truncate">
                             {project.name}
@@ -113,18 +113,18 @@ export const CommandPalette = () => {
                 {query === '' && (
                   <li className="p-4">
                     <h2 className="sr-only">Quick actions</h2>
-                    <ul className="text-sm text-gray-700">
+                    <ul className="text-sm text-gray-700 dark:text-gray-400">
                       {quickActions.map((action) => (
                         <ComboboxOption
                           as="li"
                           key={action.shortcut}
                           value={action}
-                          className="group flex cursor-default select-none items-center rounded-md px-2 py-2 data-[focus]:bg-gray-900/5 data-[focus]:text-gray-900 data-[focus]:outline-none"
+                          className="group flex cursor-default select-none items-center rounded-md px-2 py-2 data-[focus]:bg-gray-900/5 data-[focus]:text-gray-900 data-[focus]:outline-none dark:data-[focus]:bg-gray-300/5 dark:data-[focus]:text-gray-100"
                         >
                           <span className="flex-auto truncate">
                             {action.name}
                           </span>
-                          <span className="flex-none text-xs font-semibold text-gray-500">
+                          <span className="flex-none text-xs font-semibold text-gray-500 dark:text-gray-400">
                             <kbd className="font-sans">⌘</kbd>
                             <kbd className="font-sans">{action.shortcut}</kbd>
                           </span>
