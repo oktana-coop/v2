6b0fcc78df30a4168aca76da5b683cd5115fd75d
Aris Goudouras <aris.gk@gmail.com>
2025-05-14 10:43:30 +0300

    refactor: use Effect in Node filesystem adapter

--- src/modules/filesystem/adapters/electron-node-api/index.ts
+++ src/modules/filesystem/adapters/electron-node-api/index.ts
@@ -1,123 +1,141 @@
 import { promises as fs } from 'node:fs';
 import path from 'node:path';
 
+import * as Effect from 'effect/Effect';
+import { pipe } from 'effect/Function';
 import { dialog } from 'electron';
 
+import { mapErrorTo } from '../../../../utils/errors';
 import { filesystemItemTypes } from '../../constants/filesystem-item-types';
+import { AbortError, RepositoryError } from '../../errors';
 import { Filesystem } from '../../ports/filesystem';
 import { File } from '../../types';
 import { isHiddenFile } from './utils';
 
+const showDirPicker = (): Effect.Effect<
+  Electron.OpenDialogReturnValue,
+  AbortError,
+  never
+> =>
+  pipe(
+    Effect.promise(() =>
+      dialog.showOpenDialog({
+        properties: ['openDirectory'],
+      })
+    ),
+    Effect.tap((result) =>
+      result.canceled
+        ? Effect.fail(new AbortError('Open directory process cancelled'))
+        : Effect.succeed(undefined)
+    )
+  );
+
+const showSaveDialog = (
+  suggestedName: string
+): Effect.Effect<Electron.SaveDialogReturnValue, AbortError, never> =>
+  pipe(
+    Effect.promise(() =>
+      dialog.showSaveDialog({
+        defaultPath: suggestedName,
+        filters: [{ name: 'v2 Files', extensions: ['v2'] }],
+      })
+    ),
+    Effect.tap((result) =>
+      result.canceled
+        ? Effect.fail(new AbortError('Save dialog cancelled'))
+        : Effect.succeed(undefined)
+    )
+  );
+
 export const createAdapter = (): Filesystem => ({
-  openDirectory: async () => {
-    const result = await dialog.showOpenDialog({
-      properties: ['openDirectory'],
+  openDirectory: () =>
+    pipe(
+      showDirPicker(),
+      Effect.map((result) => {
+        // Get directory path and name
+        const directoryPath = result.filePaths[0];
+        const name = path.basename(directoryPath);
+
+        return {
+          type: filesystemItemTypes.DIRECTORY,
+          name,
+          path: directoryPath,
+          permissionState: 'granted', // TODO: Replace with constant
+        };
+      })
+    ),
+  getDirectory: (directoryPath: string) => {
+    const name = path.basename(directoryPath);
+
+    return Effect.succeed({
+      type: filesystemItemTypes.DIRECTORY,
+      name,
+      path: directoryPath,
+      permissionState: 'granted', // TODO: Replace with constant
     });
-
-    if (result.canceled) {
-      // TODO: Handle more gracefully with typed errors
-      throw new Error('Open directory process cancelled');
-    }
-
-    // Get directory path and name
-    const directoryPath = result.filePaths[0];
-    const name = path.basename(directoryPath);
-
-    return {
-      type: filesystemItemTypes.DIRECTORY,
-      name,
-      path: directoryPath,
-      permissionState: 'granted', // TODO: Replace with constant
-    };
   },
-  getDirectory: async (directoryPath: string) => {
-    const name = path.basename(directoryPath);
+  listDirectoryFiles: (directoryPath: string) =>
+    pipe(
+      Effect.tryPromise({
+        try: () =>
+          fs.readdir(directoryPath, {
+            withFileTypes: true,
+          }),
+        catch: mapErrorTo(RepositoryError, 'Node filesystem API error'),
+      }),
+      Effect.map((dirEntries) => {
+        const files = dirEntries
+          .filter((entry) => entry.isFile())
+          .map((entry) => {
+            const file: File = {
+              type: filesystemItemTypes.FILE,
+              name: entry.name,
+              path: path.join(directoryPath, entry.name),
+            };
 
-    return {
-      type: filesystemItemTypes.DIRECTORY,
-      name,
-      path: directoryPath,
-      permissionState: 'granted', // TODO: Replace with constant
-    };
-  },
-  listDirectoryFiles: async (directoryPath: string) => {
-    try {
-      // Read directory contents with Dirent objects
-      const dirEntries = await fs.readdir(directoryPath, {
-        withFileTypes: true,
-      });
+            return file;
+          })
+          .filter((file) => !isHiddenFile(file.path!));
 
-      // Filter out only files (excluding directories)
-      const files = dirEntries
-        .filter((entry) => entry.isFile())
-        .map((entry) => {
-          const file: File = {
-            type: filesystemItemTypes.FILE,
-            name: entry.name,
-            path: path.join(directoryPath, entry.name),
-          };
+        return files;
+      })
+    ),
+  requestPermissionForDirectory: () => Effect.succeed('granted'),
+  createNewFile: (suggestedName) => {
+    const initialContent = '';
 
-          return file;
+    return pipe(
+      showSaveDialog(suggestedName),
+      Effect.tap(({ filePath }) =>
+        Effect.tryPromise({
+          try: () => fs.writeFile(filePath, initialContent, 'utf8'),
+          catch: mapErrorTo(RepositoryError, 'Node filesystem API error'),
         })
-        .filter((file) => !isHiddenFile(file.path!));
-
-      return files;
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  },
-  requestPermissionForDirectory: async () => {
-    return 'granted';
-  },
-  createNewFile: async (suggestedName) => {
-    const { canceled, filePath } = await dialog.showSaveDialog({
-      defaultPath: suggestedName,
-      filters: [{ name: 'v2 Files', extensions: ['v2'] }],
-    });
-
-    if (canceled) {
-      // TODO: Handle more gracefully with typed errors
-      throw new Error('Save file process cancelled');
-    }
-
-    try {
-      const initialContent = '';
-      await fs.writeFile(filePath, initialContent, 'utf8');
-      const name = path.basename(filePath);
-
-      return {
+      ),
+      Effect.map(({ filePath }) => ({
         type: filesystemItemTypes.FILE,
         path: filePath,
-        name,
+        name: path.basename(filePath),
         content: initialContent,
-      };
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
+      }))
+    );
   },
-  writeFile: async (filePath: string, content: string) => {
-    try {
-      await fs.writeFile(filePath, content);
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  },
-  readFile: async (filePath: string) => {
-    try {
-      const content = await fs.readFile(filePath, 'utf8');
-
-      return {
+  writeFile: (filePath: string, content: string) =>
+    Effect.tryPromise({
+      try: () => fs.writeFile(filePath, content),
+      catch: mapErrorTo(RepositoryError, 'Node filesystem API error'),
+    }),
+  readFile: (filePath: string) =>
+    pipe(
+      Effect.tryPromise({
+        try: () => fs.readFile(filePath, 'utf8'),
+        catch: mapErrorTo(RepositoryError, 'Node filesystem API error'),
+      }),
+      Effect.map((content) => ({
         type: filesystemItemTypes.FILE,
         name: path.basename(filePath),
         path: filePath,
         content,
-      };
-    } catch (err) {
-      console.error(err);
-      throw err;
-    }
-  },
+      }))
+    ),
 });
