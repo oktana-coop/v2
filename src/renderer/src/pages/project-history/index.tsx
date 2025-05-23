5a1c1c2cf155c7b1c67bb342f4a477f32d5bf228
Aris Goudouras <aris.gk@gmail.com>
2025-05-23 15:46:09 +0300

    chore: delete old history files and fix broken storybook test

--- src/renderer/src/pages/project-history/index.tsx
+++ src/renderer/src/pages/project-history/index.tsx
@@ -3,7 +3,9 @@ import { Layout } from '../../components/layout/Layout';
 export const ProjectHistory = () => {
   return (
     <Layout>
-      <div>Project History Page Placeholder</div>
+      <div className="flex h-full w-full items-center justify-center">
+        Project History Page Placeholder
+      </div>
     </Layout>
   );
 };
