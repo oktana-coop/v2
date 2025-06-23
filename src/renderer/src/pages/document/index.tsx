94939ab5308357a2ad5f75bc46a7171440999a28
Aris Goudouras <aris.gk@gmail.com>
2025-06-24 02:25:17 +0300

    feat: enable single-doc project by default

--- src/renderer/src/pages/document/index.tsx
+++ src/renderer/src/pages/document/index.tsx
@@ -29,7 +29,7 @@ import { DocumentHistory } from './sidebar/document-history/DocumentHistory';
 import { DirectoryFiles, RecentProjects } from './sidebar/document-list-views';
 
 export const Document = () => (
-  <CurrentProjectProvider projectType={projectTypes.MULTI_DOCUMENT_PROJECT}>
+  <CurrentProjectProvider projectType={projectTypes.SINGLE_DOCUMENT_PROJECT}>
     <CurrentDocumentProvider>
       <SidebarLayoutProvider>
         <DocumentIndex />
