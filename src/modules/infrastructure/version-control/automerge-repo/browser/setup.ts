149ebb2746a498fd9f19758a3e343af301d504fb
Aris Goudouras <aris.gk@gmail.com>
2025-07-03 10:33:08 +0300

    fix: remove unneeded peer id import

--- src/modules/infrastructure/version-control/automerge-repo/browser/setup.ts
+++ src/modules/infrastructure/version-control/automerge-repo/browser/setup.ts
@@ -1,7 +1,7 @@
 // Note the ?url suffix
 import wasmUrl from '@automerge/automerge/automerge.wasm?url';
 import { next as Automerge } from '@automerge/automerge/slim';
-import { PeerId, Repo } from '@automerge/automerge-repo/slim';
+import { Repo } from '@automerge/automerge-repo/slim';
 import { IndexedDBStorageAdapter } from '@automerge/automerge-repo-storage-indexeddb';
 
 import { ElectronIPCRendererProcessAdapter } from '../electron-ipc-network-adapter/renderer';
