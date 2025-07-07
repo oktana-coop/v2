460ced637651a784c1958d0ba9f53503f88b2ba2
Aris Goudouras <aris.gk@gmail.com>
2025-07-07 17:09:12 +0300

    use type in project automerge documents
    fix: enter data properly according to the new types

--- src/modules/infrastructure/version-control/constants/versioned-artifact-types.ts
+++ src/modules/infrastructure/version-control/constants/versioned-artifact-types.ts
@@ -1,10 +1,12 @@
 import type { ValueOf } from 'type-fest';
 
-const PROJECT = 'PROJECT';
+const SINGLE_DOCUMENT_PROJECT = 'SINGLE_DOCUMENT_PROJECT';
+const MULTI_DOCUMENT_PROJECT = 'MULTI_DOCUMENT_PROJECT';
 const RICH_TEXT_DOCUMENT = 'RICH_TEXT_DOCUMENT';
 
 export const versionedArtifactTypes = {
-  PROJECT,
+  SINGLE_DOCUMENT_PROJECT,
+  MULTI_DOCUMENT_PROJECT,
   RICH_TEXT_DOCUMENT,
 } as const;
 
