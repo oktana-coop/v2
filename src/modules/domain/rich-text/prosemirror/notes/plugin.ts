2392fb1ced97811586f6a17a8f9f43c2f2d23bf6
Aris Goudouras <aris.gk@gmail.com>
2025-07-22 20:20:59 +0300

    feat: add note content numbering

--- src/modules/domain/rich-text/prosemirror/notes/plugin.ts
+++ src/modules/domain/rich-text/prosemirror/notes/plugin.ts
@@ -1,6 +1,9 @@
 import { Plugin, PluginKey } from 'prosemirror-state';
+import { Decoration, DecorationSet } from 'prosemirror-view';
 
+import { noteContentNumbering } from '../../../../../renderer/src/components/editing/inlines';
 import { createNoteNumberingTransaction } from './commands';
+import { getNotes } from './state';
 
 const pluginKey = new PluginKey('note-numbering');
 
@@ -19,5 +22,34 @@ export const notesPlugin = () =>
 
       const tr = createNoteNumberingTransaction(newState);
       return tr.steps.length ? tr : null;
+    },
+    props: {
+      decorations(state) {
+        const { contentBlocks } = getNotes(state.doc);
+        const decorations = [];
+
+        for (let i = 0; i < contentBlocks.length; i++) {
+          const content = contentBlocks[i];
+          const id = content.node.attrs.id;
+
+          if (id) {
+            const deco = Decoration.widget(
+              // +2 to be before the content of the first paragraph of the note (which is a div itself)
+              content.pos + 2,
+              () => {
+                const span = document.createElement('span');
+                span.textContent = `${id}: `;
+                span.className = noteContentNumbering;
+                span.style.userSelect = 'none';
+                span.style.pointerEvents = 'none';
+                return span;
+              },
+              { side: -1 }
+            );
+            decorations.push(deco);
+          }
+        }
+        return DecorationSet.create(state.doc, decorations);
+      },
     },
   });
