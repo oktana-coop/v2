4a4ed8fa85612889d31d186f55cf7c91640aa699
Aris Goudouras <aris.gk@gmail.com>
2025-09-22 14:38:20 +0300

    feat: open file from OS event

--- src/modules/infrastructure/filesystem/index.ts
+++ src/modules/infrastructure/filesystem/index.ts
@@ -2,3 +2,4 @@ export * from './utils';
 export * from './types';
 export * from './errors';
 export * from './ports/filesystem';
+export * from './constants/filesystem-item-types';
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,7 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ElectronProvider } from '../../modules/infrastructure/cross-platform';
+import { type File } from '../../modules/infrastructure/filesystem';
 import { AppWrapper } from './AppWrapper';
+
+// Listen for files opened from the OS and store them in session storage
+// to be picked up by the relevant project context
+// This is necessary because the context may not be mounted yet when the event is received
+window.osEventsAPI.onOpenFileFromFilesystem((file: File) => {
+  sessionStorage.setItem('fileToBeOpened', JSON.stringify(file));
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
