ef755d2b324e0383be8e3701643802c02f83309c
Aris Goudouras <aris.gk@gmail.com>
2025-09-30 14:31:15 +0300

    refactor: define the VersionedArtifact without importing from Automerge

--- src/modules/infrastructure/version-control/models/artifact.ts
+++ src/modules/infrastructure/version-control/models/artifact.ts
@@ -1,15 +1,17 @@
-import { next as Automerge } from '@automerge/automerge/slim';
 import {
   type DocHandle as AutomergeDocHandle,
   type DocHandleChangePayload as AutomergeDocHandleChangePayload,
 } from '@automerge/automerge-repo/slim';
 
-export type VersionedArtifact<ArtifactType> = Automerge.Doc<ArtifactType>;
+// This is the same with Automerge.Doc
+// https://github.com/automerge/automerge/blob/main/javascript/src/types.ts#L29
+// But we re-define it here so that we can re-use it for other version control systems without introducing confusion.
+export type VersionedArtifact<ArtifactType> = {
+  readonly [P in keyof ArtifactType]: ArtifactType[P];
+};
 
 export type VersionedArtifactHandle<ArtifactType> =
   AutomergeDocHandle<ArtifactType>;
-
-export type VersionedArtifactPatch = Automerge.Patch;
 
 export type VersionedArtifactHandleChangePayload<T> =
   AutomergeDocHandleChangePayload<T>;
