2e3e1ca3b0ef1c36a4fdb6dfcc8f4854da1896ac
Aris Goudouras <aris.gk@gmail.com>
2025-10-31 11:03:22 +0200

    chore: delete unneeded command

--- src/modules/domain/project/commands/single-document-project/index.ts
+++ src/modules/domain/project/commands/single-document-project/index.ts
@@ -1,3 +1,2 @@
 export { createDocumentAndProject } from './create-document-and-project';
-export { getProjectDocument } from './get-project-document';
 export { getProjectName } from './get-project-name';
