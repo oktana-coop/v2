9ac0e24fe50a931a79c3c00cbdd131c47c9cbff8
Aris Goudouras <aris.gk@gmail.com>
2025-12-09 14:47:31 +0200

    docs: add TODO for update notifications refactoring

--- src/renderer/src/components/notifications/Notifications.tsx
+++ src/renderer/src/components/notifications/Notifications.tsx
@@ -4,6 +4,7 @@ import { UpdateNotification } from './UpdateNotification';
 
 export const Notifications = () => (
   <NotificationsContainer>
+    {/* TODO: Incorporate update notifications to the generic notifications mechanism */}
     <UpdateNotification />
     <GenericNotifications />
   </NotificationsContainer>
