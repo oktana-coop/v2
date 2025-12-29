25937bf0574d0690f2761ec66e0c027f44dfa81c
Aris Goudouras <aris.gk@gmail.com>
2025-12-29 16:20:42 +0200

    feat: add an empty directory view (multi-doc projects)

--- src/renderer/src/pages/project/shared/document-list-views/directory-files/index.tsx
+++ src/renderer/src/pages/project/shared/document-list-views/directory-files/index.tsx
@@ -1,13 +1,44 @@
 import { useContext } from 'react';
 
+import { type Directory } from '../../../../../../../modules/infrastructure/filesystem';
 import { MultiDocumentProjectContext } from '../../../../../app-state';
 import { IconButton } from '../../../../../components/actions/IconButton';
 import { FolderIcon, PlusIcon } from '../../../../../components/icons';
 import { SidebarHeading } from '../../../../../components/sidebar/SidebarHeading';
-import { useCreateDocument, useDocumentList } from '../../../../../hooks';
+import {
+  type DocumentListItem,
+  useCreateDocument,
+  useDocumentList,
+} from '../../../../../hooks';
 import { useDocumentSelection as useDocumentSelectionInMultiDocumentProject } from '../../../../../hooks/multi-document-project';
 import { DocumentList } from '../DocumentList';
+import { EmptyView } from './EmptyView';
 import { NoActiveDirectoryView } from './NoActiveDirectoryView';
+
+const DirectoryFilesList = ({
+  directory,
+  documents,
+  onCreateDocument,
+  onSelectItem,
+}: {
+  directory: Directory | null;
+  documents: DocumentListItem[];
+  onCreateDocument: () => void;
+  onSelectItem: (id: string) => Promise<void>;
+}) => {
+  if (documents.length > 0) {
+    return (
+      <div className="flex flex-col items-stretch overflow-auto">
+        <div className="mb-1 truncate px-4 text-left font-bold text-black text-opacity-85 dark:text-white dark:text-opacity-85">
+          {directory?.name ?? 'Files'}
+        </div>
+        <DocumentList items={documents} onSelectItem={onSelectItem} />
+      </div>
+    );
+  }
+
+  return <EmptyView onCreateDocumentButtonClick={onCreateDocument} />;
+};
 
 export const DirectoryFiles = ({
   onCreateDocument,
@@ -36,15 +67,12 @@ export const DirectoryFiles = ({
       </div>
 
       {canShowList ? (
-        <div className="flex flex-col items-stretch overflow-auto">
-          <div className="mb-1 truncate px-4 text-left font-bold text-black text-opacity-85 dark:text-white dark:text-opacity-85">
-            {directory?.name ?? 'Files'}
-          </div>
-          <DocumentList
-            items={documents}
-            onSelectItem={handleDocumentSelection}
-          />
-        </div>
+        <DirectoryFilesList
+          directory={directory}
+          documents={documents}
+          onCreateDocument={onCreateDocument}
+          onSelectItem={handleDocumentSelection}
+        />
       ) : (
         <NoActiveDirectoryView />
       )}
