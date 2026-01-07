8b2275bae09c9262f0660a4facde5b539e2ad110
Aris Goudouras <aris.gk@gmail.com>
2026-01-07 16:26:35 +0200

    feat: add branch to merge conflict info

--- src/modules/infrastructure/version-control/models/merge/merge-conflict-info.ts
+++ src/modules/infrastructure/version-control/models/merge/merge-conflict-info.ts
@@ -1,11 +1,14 @@
+import { type Branch } from '../branch';
 import { type Commit } from '../commit';
 import { type MergeConflict } from './merge-conflict';
 
 export type MergeConflictInfo = {
   // Aka "ours" in Git.
   targetCommitId: Commit['id'];
+  targetCommitBranch?: Branch;
   // Aka "their" in Git.
   sourceCommitId: Commit['id'];
+  sourceCommitBranch?: Branch;
   // Aka "base" in Git.
   commonAncestorCommitId: Commit['id'];
   conflicts: MergeConflict[];
