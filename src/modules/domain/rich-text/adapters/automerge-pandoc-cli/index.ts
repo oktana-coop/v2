78c7db437e776e7095fd0d93c063aeb77a20c615
Aris Goudouras <aris.gk@gmail.com>
2026-02-01 12:01:24 +0200

    feat: handle errors from Haskell lib

--- src/modules/domain/rich-text/adapters/automerge-pandoc-cli/index.ts
+++ src/modules/domain/rich-text/adapters/automerge-pandoc-cli/index.ts
@@ -5,6 +5,26 @@ import {
 import { type RepresentationTransform } from '../../ports/representation-transform';
 import { representationToCliArg } from './cli-args';
 
+type HSLibConversionSuccessOutput = {
+  data: string;
+};
+
+type HSLibError = {
+  message: string;
+};
+
+type HSLibFailureOutput = {
+  errors: HSLibError[];
+};
+
+type HSLibConversionOutput = HSLibConversionSuccessOutput | HSLibFailureOutput;
+
+const isHSLibFailureOutput = (
+  output: HSLibConversionOutput
+): output is HSLibFailureOutput => {
+  return 'errors' in output;
+};
+
 export const createAdapter = ({
   runWasiCLIOutputingText,
   runWasiCLIOutputingBinary,
@@ -34,11 +54,16 @@ export const createAdapter = ({
       ],
     });
 
-    if (!output) {
-      throw new Error('Error in transforming to text. No output received.');
+    // TODO: Perform proper validation & handle error cases
+    const parsedOutput = JSON.parse(output) as HSLibConversionOutput;
+
+    if (isHSLibFailureOutput(parsedOutput)) {
+      throw new Error(
+        `Conversion failed: ${parsedOutput.errors.map((error) => error.message).join(', ')}`
+      );
     }
 
-    return output;
+    return parsedOutput.data;
   };
 
   const transformToBinary: RepresentationTransform['transformToBinary'] =
