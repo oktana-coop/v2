37c2e9b890ba3315d81c6fd16ad7c1fb0bb39277
Stathis Moraitidis <emoraitidis@gmail.com>
2026-04-10 15:54:16 +0100

    feat: add folder icon (#295)

--- src/renderer/src/pages/project/shared/explorer-tree-views/directory-tree/index.tsx
+++ src/renderer/src/pages/project/shared/explorer-tree-views/directory-tree/index.tsx
@@ -110,7 +110,7 @@ export const DirectoryTreeView = ({
 }: {
   onCreateDocument: () => void;
 }) => {
-  const { directory } = useContext(MultiDocumentProjectContext);
+  const { directory, openDirectory } = useContext(MultiDocumentProjectContext);
   const handleDocumentSelection = useDocumentSelectionInMultiDocumentProject();
   const {
     explorerTree: documents,
@@ -157,6 +157,11 @@ export const DirectoryTreeView = ({
           <SidebarHeading icon={FolderIcon} text="File Explorer" />
         </div>
         <div className="flex gap-1">
+          <IconButton
+            onClick={() => openDirectory()}
+            icon={<FolderIcon size={20} />}
+            tooltip="Open Folder"
+          />
           {canCreateDocument && (
             <IconButton
               onClick={onCreateDocument}
