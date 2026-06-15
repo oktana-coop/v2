393d7af7843775091ee07e1aeb27836243bf944a
Aris Goudouras <aris.gk@gmail.com>
2026-06-15 15:39:54 +0300

    feat: update document asset src type

--- src/modules/domain/rich-text/models/document-asset.ts
+++ src/modules/domain/rich-text/models/document-asset.ts
@@ -1,8 +1,8 @@
-import { type AssetDocRelPath } from './document-asset-src';
+import { type DocumentAssetSrc } from './document-asset-src';
 
 // An asset reference as it appears in a rich-text document.
 export type DocumentAsset = {
-  src: AssetDocRelPath;
+  src: DocumentAssetSrc;
   alt: string | null;
   title: string | null;
 };
