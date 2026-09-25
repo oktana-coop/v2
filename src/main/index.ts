61b6fd32455382f3e7dfa2e97264e98fd2815929
Aris Goudouras <aris.gk@gmail.com>
2026-09-25 15:46:08 +0300

    fix: load renderer from the running build directory

--- src/main/index.ts
+++ src/main/index.ts
@@ -84,7 +84,7 @@ globalThis.__dirname = dirname(__filename);
 // │ └─┬ renderer
 // │   └── index.mjs
 process.env.DIST_ELECTRON = join(__dirname, '../');
-process.env.DIST = join(process.env.DIST_ELECTRON, '../dist');
+process.env.DIST = process.env.DIST_ELECTRON;
 process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
   ? join(process.env.DIST_ELECTRON, '../public')
   : process.env.DIST;
